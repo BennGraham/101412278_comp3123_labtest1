fe058523c4bddc7af3a32960ddc7c0fd0770dba7
Benn <benngraham90@gmail.com>
2024-10-03 18:08:49 -0400

    Question 3

---
+++ Question3/file_module.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+
+const delete_or_create_logs = () => {
+  const logs_directory = "./Logs";
+
+  if (fs.existsSync(logs_directory)) {
+    console.log(`${logs_directory} found. Processing log files...`);
+
+    fs.readdirSync(logs_directory).forEach((file) => {
+      fs.unlinkSync(`${logs_directory}/${file}`);
+      console.log(`Deleted file: ${file}`);
+    });
+
+    fs.rmdirSync(logs_directory);
+    console.log(`${logs_directory} deleted.`);
+  } else {
+    console.log(
+      `${logs_directory} not found, creating it and adding log files.`
+    );
+    fs.mkdirSync(logs_directory);
+
+    process.chdir(logs_directory);
+
+    const newFiles = [];
+    for (let i = 1; i <= 10; i++) {
+      const fileName = `log${i}.txt`;
+      const content = `This is log file ${i}`;
+      fs.writeFileSync(fileName, content);
+      newFiles.push(fileName);
+    }
+
+    console.log("Created files:");
+    newFiles.forEach((file) => console.log(file));
+
+    process.chdir("..");
+  }
+};
+
+delete_or_create_logs();
